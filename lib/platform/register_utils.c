d630d995f580fa8fea433052c31837af6ffff63b
MarcoAAG <marco-antonioa@outlook.com>
2023-06-01 22:55:20 -0600

    feat: Add utils
    related MSP-01

---
+++ lib/platform/register_utils.c
@@ -0,0 +1,10 @@
+#include <MSP430_Registers.h>
+#include <register_utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
---
+++ lib/platform/register_utils.h
@@ -0,0 +1,14 @@
+#ifndef REGISTER_UTILS_H
+#define REGISTER_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
