59de8c0792585a592893bf155f345983177482e5
MarcoAAG <marco-antonioa@outlook.com>
2023-06-01 18:58:53 -0600

    feat: Add lib
    related to MSP-01

--- src/library.c
+++ src/library.c
@@ -1,8 +1,14 @@
 #include <MSP430_Registers.h>
+#include <Dio.h>
 
 void main()
 {
     WDTCTL = 0x5A80 | 0x0080;   // Stop watchdog timer
+    DIO_v_init();
+    (void)DIO_u_configPin(bit0, port1, output, gpio, disabled);
+    (void)DIO_u_configPin(bit6, port1, output, gpio, disabled);
+    (void)DIO_u_setPinState(bit0, port1, high);
+    (void)DIO_u_setPinState(bit6, port1, high);
     while (1)
     {
         
