938a0dc727ef08e494ef3c4f10e1431c712383bc
MarcoAAG <marco-antonioa@outlook.com>
2023-07-25 20:49:39 -0600

    feat: Add BCM module
    related to #11

---
+++ lib/bcm/include/bcm.h
@@ -0,0 +1,28 @@
+/**********************************************************************************************************************
+ * \file bcm.h
+ *
+ * \author  Marco Aguilar
+ *
+ * \date Jul-25-2023
+ *
+ * \version 1.0 \n \n
+ *
+ * This file contains:
+ *  - Definitions
+ *  - Types
+ *  - Interface Prototypes
+ *  which are relevant for the BCM Driver
+ *********************************************************************************************************************/
+
+#ifndef BCM_H
+#define BCM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/bcm/src/bcm.c
@@ -0,0 +1,23 @@
+/**********************************************************************************************************************
+ * \file bcm.c
+ *
+ * \author  Marco Aguilar
+ *
+ * \date Jul-25-2023
+ *
+ * \version 1.0 \n \n
+ *
+ * This file contains:
+ *  - Definitions
+ *  - Types
+ *  - Interface Prototypes
+ *  which are relevant for the BCM Driver
+ *********************************************************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
