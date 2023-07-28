19baac8ab57799454038d475781535c06523d0af
MarcoAAG <marco-antonioa@outlook.com>
2023-07-27 21:45:28 -0600

    feat: Add DCO
    related to #11

--- lib/bcm/include/bcm.h
+++ lib/bcm/include/bcm.h
@@ -21,6 +21,28 @@
 extern "C" {
 #endif
 
+typedef enum
+{
+    freq_1Mhz = (uint8)0u,
+    freq_8Mhz = (uint8)1u,
+    freq_12Mhz = (uint8)2u,
+    freq_16Mhz = (uint8)3u
+
+}t_Frequency;
+
+typedef enum
+{
+    div_1 = (uint8)0u,
+    div_2 = (uint8)1u,
+    div_3 = (uint8)2u,
+    div_4 = (uint8)3u
+}t_Divider;
+
+void BCM_v_DCOConf(t_Frequency e_freq);
+void BCM_v_MCLKConf();
+void BCM_v_SMCLKConf();
+void BCM_v_ACLKConf(t_Frequency e_freq, t_Divider e_div);
+
 #ifdef __cplusplus
 }
 #endif
--- lib/bcm/src/bcm.c
+++ lib/bcm/src/bcm.c
@@ -14,9 +14,26 @@
  *  which are relevant for the BCM Driver
  *********************************************************************************************************************/
 
+#include <msp430g2553_registers.h>
+#include <register_utils.h>
+#include <bcm.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+volatile uint16* const pu_CALDCO_XMHZ_ADDR[4]  = { u_CALDCO_1MHZ_ADDR, u_CALDCO_8MHZ_ADDR, \
+                                                  u_CALDCO_12MHZ_ADDR, u_CALDCO_16MHZ_ADDR };
+
+void BCM_v_DCOConf(t_Frequency e_freq)
+{
+    REG_v_WriteToReg(u_DCOCTL_ADDR,REG_u_ReadFromReg(pu_CALDCO_XMHZ_ADDR[e_freq]));
+}
+
+void BCM_v_ACLKConf(t_Frequency e_freq, t_Divider e_div)
+{
+}
+
 
 #ifdef __cplusplus
 }
