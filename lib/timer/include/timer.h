ba378e1382452df423addb6d3239b36bda97aab9
MarcoAAG <marco-antonioa@outlook.com>
2023-08-17 18:58:06 -0600

    feat: Setup timer driver
    related to #17

---
+++ lib/timer/include/timer.h
@@ -0,0 +1,28 @@
+/**********************************************************************************************************************
+ * \file timer.h
+ *
+ * \author  Marco Aguilar
+ *
+ * \date Aug-17-2023
+ *
+ * \version 1.0 \n \n
+ *
+ * This file contains:
+ *  - Definitions
+ *  - Types
+ *  - Interface Prototypes
+ *  which are relevant for the Timer Driver
+ *********************************************************************************************************************/
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ lib/timer/src/timer.c
@@ -0,0 +1,25 @@
+/**********************************************************************************************************************
+ * \file timer.c
+ *
+ * \author  Marco Aguilar
+ *
+ * \date Aug-17-2023
+ *
+ * \version 1.0 \n \n
+ *
+ * This file contains:
+ *  - Definitions
+ *  - Types
+ *  - Interface Prototypes
+ *  which are relevant for the Timer Driver
+ *********************************************************************************************************************/
+
+#include <timer.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
