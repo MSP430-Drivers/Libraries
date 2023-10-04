de134adc2f84505293f09292a8c648c6c2dd0784
MarcoAAG <marco-antonioa@outlook.com>
2023-10-03 20:44:48 -0600

    feat: Add init functions
    related to #17

--- lib/timer/include/timer.h
+++ lib/timer/include/timer.h
@@ -17,9 +17,50 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <std_types.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef enum
+{
+  timerA0 = (uint8)0u,
+  timerA1 = (uint8)1u,
+  TimerNC = (uint8)2u
+} t_TimerChannel;
+
+typedef enum
+{
+  stop       = (uint8)0u,
+  up         = (uint8)1u,
+  continuous = (uint8)2u,
+  updown     = (uint8)3u,
+} t_TimerMode;
+
+typedef enum
+{
+  capture = (uint8)0u,
+  compare = (uint8)1u,
+  NC      = (uint8)3u
+} t_TimerType;
+
+typedef enum
+{
+  slot0 = (uint8)0u,
+  slot1 = (uint8)1u,
+  slot2 = (uint8)2u
+} t_TimerSlot;
+
+typedef struct
+
+{
+  t_TimerChannel timer;
+  t_TimerMode    mode;
+  t_TimerType    slot[3];
+} t_Timers;
+
+static t_Timers timersChannels[2];
 
 #ifdef __cplusplus
 }
--- lib/timer/src/timer.c
+++ lib/timer/src/timer.c
@@ -14,11 +14,53 @@
  *  which are relevant for the Timer Driver
  *********************************************************************************************************************/
 
+#include <msp430g2553_registers.h>
 #include <timer.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+volatile uint16* const pu_TAxCTL_ADDR[2] = { TA0CTL_ADDR, TA1CTL_ADDR };
+volatile uint16* const pu_TAxR_ADDR[2]   = { TA0R_ADDR, TA1R_ADDR };
+
+void TIMER_v_Init(void)
+{
+  timersChannels[0].timer   = TimerNC;
+  timersChannels[0].slot[0] = NC;
+  timersChannels[0].slot[1] = NC;
+  timersChannels[0].slot[2] = NC;
+  timersChannels[1].timer   = TimerNC;
+  timersChannels[1].slot[0] = NC;
+  timersChannels[1].slot[1] = NC;
+  timersChannels[1].slot[2] = NC;
+}
+
+void TIMER_v_Configure(t_TimerChannel e_timer, t_TimerMode e_mode)
+{
+  if(timersChannels[e_timer].timer != TimerNC)
+  {
+    timersChannels[e_timer].timer = e_timer;
+    timersChannels[e_timer].mode  = e_mode;
+  }
+  else
+  {
+    // IED Implementation Error Detection
+    // Timer is already configured
+  }
+}
+
+void TIMER_v_SetInstance(t_TimerChannel e_timer, t_TimerType e_type, t_TimerSlot e_slot)
+{
+  if(timersChannels[e_timer].slot[e_slot] != NC)
+  {
+  }
+  else
+  {
+    // IED Implementation Error Detection
+    // Timer Slot is already configured
+  }
+}
 
 #ifdef __cplusplus
 }
